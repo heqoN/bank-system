18b6ddb1c7d76fda047e6ad634470cd1869b6482
Abdullah Aydın <87316307+heqoN@users.noreply.github.com>
2025-08-26 18:08:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,37 @@
+import sys
+import time
 
+def createFiles():
+  pass
+
+def exit():
+  print("  Çıkış yapılıyor ...")
+  time.sleep(3)
+  sys.exit()
+
+def register():
+  pass
+
+def login():
+  pass
+
+def adminlogin():
+  pass
+
+
+
+
+print("by heqoN".center(100,"-")
+print("  Banka sistemi projeme hoşgeldiniz.")
+order1 = input("SEÇENEKLER".center(25,"*")+"\n\n   Kayıt ol -1\n  Giriş yap -2\n Çıkış yap -3\n  Admin girişi  -4\n\n   >>> ")
+if order1 == "1" :
+  register()
+elif order1 == "2" :
+  login()
+elif order1 == "3" :
+  exit()
+elif order1 == "4" :
+  adminlogin()
+else :
+  print("  Bir hata oluştu.")
+  exit()
